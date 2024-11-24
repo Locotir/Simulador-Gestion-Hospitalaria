089eddacba785b6859398c270c2783a5a4c20585
Locotir <magazo2005@gmail.com>
2024-11-24 21:31:05 +0100

    clase bcolors definida

---
+++ src/main.hpp
@@ -0,0 +1,22 @@
+#ifndef BCOLORS_H
+#define BCOLORS_H
+#include <string>
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++ Clase de Codigos de Color ++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+class bcolors {
+public:
+    static const std::string PURPLE;
+    static const std::string BLUE;
+    static const std::string BLUEL;
+    static const std::string GREEN;
+    static const std::string YELLOW;
+    static const std::string RED;
+    static const std::string UNDERLINE;
+    static const std::string WHITE;
+    static const std::string ORANGE;
+    static const std::string VIOLET;
+    static const std::string BLACK;
+    static const std::string RESET;
+};
+
+#endif
