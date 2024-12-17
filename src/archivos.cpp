19014ed562a23baa56478ecf2c53cb69bc00b37f
Locotir <magazo2005@gmail.com>
2024-12-17 16:27:27 +0100

    Del

--- src/archivos.cpp
+++
@@ -1,9 +0,0 @@
-/*
-TODO:
-- Guardar los datos en archivos {paciente, medicos, citas y servicios}
-- Funcionalidad de backup de la base de datos
-*/
-
-void guardarDatosEnArchivo();
-void cargarDatosDesdeArchivo();
-void realizarBackup();
